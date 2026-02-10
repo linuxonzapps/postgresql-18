b78f2230967d44ada0c920469f035274818443ad
rposts <rposts>
2026-02-10 18:55:42 +0000

    Fix script execution

--- src/build.sh
+++ src/build.sh
@@ -5,7 +5,6 @@ version=${arr[1]}
 trap 0 1 2 ERR
 # Ensure sudo is installed
 current_dir="$PWD"
-sudo sed -E -i '/sudo useradd|sudo passwd/d' /tmp/linux-on-ibm-z-scripts/PostgreSQL/${version}/build_postgresql.sh
 bash /tmp/linux-on-ibm-z-scripts/PostgreSQL/${version}/build_postgresql.sh -y
 cd $PWD/postgresql-${version}/tmp_install/ && tar cvfz postgresql-${version}-linux-s390x.tar.gz initdb-template usr
 mv postgresql-${version}-linux-s390x.tar.gz ${current_dir}
