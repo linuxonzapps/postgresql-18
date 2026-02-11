ed3de74b67bd504be62479dbe5b325763786cf4c
rposts <rposts>
2026-02-11 18:48:22 +0000

    Fix tar file

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,6 @@ trap 0 1 2 ERR
 apt-get update && apt-get install sudo -y
 current_dir="$PWD"
 bash /tmp/linux-on-ibm-z-scripts/PostgreSQL/${version}/build_postgresql.sh -y
-cd /usr/local/pgsql/ && tar cvfz postgresql-${version}-linux-s390x.tar.gz *
+cd /usr/local/pgsql/ && tar cvfz postgresql-18-${version}-linux-s390x.tar.gz *
 mv postgresql-18-${version}-linux-s390x.tar.gz ${current_dir}
 exit 0
