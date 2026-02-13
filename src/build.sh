0390c451366f677782a085c15ad4053b227bad12
rposts <rposts>
2026-02-13 21:01:47 +0000

    Add DISTRO tag

--- src/build.sh
+++ src/build.sh
@@ -5,7 +5,16 @@ version=${arr[1]}
 trap 0 1 2 ERR
 # Ensure sudo is installed
 apt-get update && apt-get install sudo -y
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$DISTRO-$VERSION_CODENAME"
+    fi
+fi
 current_dir="$PWD"
+echo $DISTRO > .distro_zab.txt
 bash /tmp/linux-on-ibm-z-scripts/PostgreSQL/${version}/build_postgresql.sh -y
 cd /usr/local/pgsql/ && tar cvfz postgresql-18-${version}-linux-s390x.tar.gz *
 mv postgresql-18-${version}-linux-s390x.tar.gz ${current_dir}
